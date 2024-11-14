b7e5bc40913ac51f6e352d3f87eedacaa5896724
Diego Zito <81996889+diegozitto@users.noreply.github.com>
2024-11-14 11:37:30 -0300

    Update Aula6_AreaCirc.java [ from Santander ]

--- src/Modulo3_Classes_E_Metodos/aulas/Aula6_AreaCirc.java
+++ src/Modulo3_Classes_E_Metodos/aulas/Aula6_AreaCirc.java
@@ -1,4 +1,17 @@
 package Modulo3_Classes_E_Metodos.aulas;
 
 public class Aula6_AreaCirc {
+  double raio;
+  double pi;
+
+  Aula6_AreaCirc(double raioInicial){
+    pi = 3.14;
+    raio = raioInicial;
+  }
+
+  double area(){
+    return pi * Math.pow(raio,2);
+  }
+    
+  
 }
